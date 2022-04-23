ebbc861bc735f62202d958ed21732f0692225587
codekeyur <keyurpatel2868@gmail.com>
2022-04-23 16:57:05 -0400

    Server script updated

--- ec2/ec2_windows_server_2016.py
+++ ec2/ec2_windows_server_2016.py
@@ -1,41 +1,45 @@
 #!/usr/bin/env python3
 
+from time import sleep
 import boto3
 from  botocore.exceptions import ClientError
 
-DRYRUN = False
+resource=boto3.resource('ec2')
+ec2_client=boto3.client('ec2') 
+vpcs = ec2_client.describe_vpcs()
+vpc_id = vpcs.get('Vpcs', [{}])[0].get('VpcId','')
+subnets = ec2_client.describe_subnets()
+subnet_id = subnets['Subnets'][0]['SubnetId']
 
+DRYRUN = False
 print("-----------------------------------------------")
 print("-------Launch Windows Server 2016 on AWS-------")
 print("-----------------------------------------------")
 print("In order to connect to your instance you will need key-pair please enter 1 to create new key-pair oe 2 to continue to use existing key-pair")
+sleep(5)
 print("""
 1 - Create new key pair
 2 - To continue and use existing key-pair
 """)
 ####----Creating Key pair and Save pem file to current wdir----####
 #-----------------------------------------------------------------#
-ec2_client=boto3.client('ec2')
+
 def create_key_pair():
      key_pair=ec2_client.create_key_pair(KeyName='Windows-key')
      private_key=key_pair['KeyMaterial']
      with open('Windows-key.pem', 'w') as f:
        f.write(private_key)
-     
-userInputKeyPair=int(input())
 
+userInputKeyPair=int(input())
 if userInputKeyPair == 1:
    create_key_pair()
 
 print("------Creating Security Group------")
 print("-----------------------------------")
 
-response = ec2_client.describe_vpcs()
-vpc_id = response.get('Vpcs', [{}])[0].get('VpcId','')
-try:
-  response = ec2_client.create_security_group(
-                    GroupName="Windows Server Security Group",
-                    Description="Windows Server Sg",
+security_group = resource.create_security_group(
+                    GroupName="Windows Server Desktop Experience Security Group",
+                    Description="Windows Server Desktop Sg Standard Base",
                     VpcId=vpc_id,
                     TagSpecifications=[
                                 {
@@ -43,34 +47,31 @@ try:
                                     'Tags': [
                                                {
                                                    'Key': 'Name',
-                                                   'Value': 'Windows Server Sg'
+                                                   'Value': 'Windows Server Desktop Exp Standard Base Server'
                                                },
                                              ]
                                 }      
                               ]
                            ) 
-  security_group_id=response['GroupId']
-  print("Security Group created %s in vpc %s." % (security_group_id,vpc_id))
+print("Security Group created %s " % security_group.group_id)
   
 
 ####----Define Security Rules----####
 #-----------------------------------#
-  data = ec2_client.authorize_security_group_ingress(
-      GroupId=security_group_id,
+
+data = security_group.authorize_ingress(
+      GroupId=security_group.group_id,
       IpPermissions=[
           { 'IpProtocol': 'tcp',
             'FromPort': 80,
             'ToPort': 80,
             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
           { 'IpProtocol': 'tcp',
-            'FromPort': 22,
-            'ToPort': 22,
+            'FromPort': 3389,
+            'ToPort': 3389,
             'IpRanges' : [{'CidrIp': '0.0.0.0/0'}]}
           ])
-  print("Ingress Successfully set %s." % data)
-
-except ClientError as e:
-  print(e)
+print("Ingress Rules Successfully set.")
 
 #to see all the images available on AWS owned by amazon.
 images = ec2_client.describe_images(
@@ -78,7 +79,7 @@ images = ec2_client.describe_images(
         {
             'Name':'name',
             'Values': [
-                'Windows_Server-2016-English-Core-Base*',
+                'Windows_Server-2016-English-Full-*',
             ]
         },
         {
@@ -99,17 +100,26 @@ print("----------------------------------------------")
 print("------Select which image you want to use------")
 print("----------------------------------------------")
 
+sleep(3)
+
 userInput=int(input())
 
-if userInput == 1:
-   imageid=images['Images'][0]['ImageId']
-   instance = ec2_client.run_instances(
+if 1 <= userInput <=100 :
+   imageid=images['Images'][10]['ImageId']
+   instance = resource.create_instances(
        ImageId=imageid,
        InstanceType = 't2.micro',
        KeyName="Windows-key",
        MaxCount=1,
        MinCount=1,
-       SecurityGroupIds=security_group_id,
+       NetworkInterfaces=[{
+         'SubnetId':subnet_id,
+         'DeviceIndex': 0,
+         'AssociatePublicIpAddress': True,
+         'Groups': [
+           security_group.group_id
+         ]
+       }],
        DryRun=DRYRUN,
        TagSpecifications=[
         {
@@ -123,10 +133,12 @@ if userInput == 1:
         }
       ]
     )
-   print("Instance Successfully Launched")     
+   print(instance)
+   #print("Instance Successfully Launched.")  
+   #sleep(120)
+   #print("Instance PublicDNS: %s " % instance.public_dns_name)   
 else:
    print("Luanch failed !!")
 
-#verify the instance launch using correct imageId
 
 
---
+++ ec2/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+userInput=int(input("Enter any number between 1-5 : "))
+print(type(userInput))
+if 1 <= userInput <= 5 :
+   print("Test Success !!")
+else:
+   print("Still now working !!")
+
+
